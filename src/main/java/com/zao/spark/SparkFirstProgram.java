aad8a599252f00431b9dfb1cb22a21e5a6618d95
zaodhamana <muhorondungu@zaodhamana.com>
2024-12-27 00:51:34 +0300

    target update and first program and log4j date issues

--- src/main/java/com/zao/spark/SparkFirstProgram.java
+++ src/main/java/com/zao/spark/SparkFirstProgram.java
@@ -1,5 +1,8 @@
 package com.zao.spark;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
@@ -7,11 +10,19 @@ public class SparkFirstProgram {
 
 	public static void main(String[] args) {
 		try (final var spark = SparkSession.builder()
-										   .appName("SparkFirstProgram")
-										   .master("local[*]")
-										   .getOrCreate();
+			 	.appName("SparkFirstProgram")
+				.master("local[*]")
+				.getOrCreate();
 				final var sc = new JavaSparkContext(spark.sparkContext())) {
+			final var data = Stream.iterate(1, n -> n + 1)
+					.limit(5)
+					.collect(Collectors.toList());
+			final var myRdd = sc.parallelize(data);
+			System.out.printf("Total elements in RDD: %d%n", myRdd.count());
 
+		}catch (Exception e) {
+			e.getMessage();
+			
 		}
 	}
 
