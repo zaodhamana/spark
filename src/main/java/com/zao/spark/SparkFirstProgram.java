a4f7201690b5ac0ff6626d3e3ff3b8ce7fcd6401
zaodhamana <muhorondungu@zaodhamana.com>
2024-12-27 18:20:03 +0300

    Log4j2 set up and providing some serious feedback on Spark

--- src/main/java/com/zao/spark/SparkFirstProgram.java
+++ src/main/java/com/zao/spark/SparkFirstProgram.java
@@ -10,15 +10,22 @@ public class SparkFirstProgram {
 
 	public static void main(String[] args) {
 		try (final var spark = SparkSession.builder()
-			 	.appName("SparkFirstProgram")
+				.appName("SparkFirstProgram")
 				.master("local[*]")
 				.getOrCreate();
 				final var sc = new JavaSparkContext(spark.sparkContext())) {
 			final var data = Stream.iterate(1, n -> n + 1)
-					.limit(5)
+					.limit(500)
 					.collect(Collectors.toList());
 			final var myRdd = sc.parallelize(data);
 			System.out.printf("Total elements in RDD: %d%n", myRdd.count());
+			System.out.printf("Default number of partitions: %d%n", myRdd.getNumPartitions());
+			
+			final var max = myRdd.reduce(Integer::max);
+			final var min = myRdd.reduce(Integer::min);
+			final var sum = myRdd.reduce(Integer::sum);
+			
+			System.out.printf("Max->%d, Min->%d, Sum->%d", max, min, sum);
 
 		}
 	}
